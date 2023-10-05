b1c2f4aeef93ba49b57eaa5a394113c752b696af
Waqas Khan <71628657+Waqaskhan404@users.noreply.github.com>
2023-10-05 23:46:48 +0500

    dbConnection Setup Now

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ const client=new MongoClient(url);
 
 const dbName='myDataBase';
 
-async function main(){
+// If we make all calls here like find in the one file   it will make confusion lets make different files for each call setup things
+
+const dbConnection=async()=>{
+    try{
     await client.connect();
     console.log("Connected Successfully to the server");
     const db=client.db(dbName);
-    const collection=db.collection("Users");
-
-    const findResult=await collection.find().toArray();
-    console.log(findResult);
+    return db;
+    }
+    catch(error){
+        console.error("Failed to connect to the server :",error)
+    }
+    
+// We will make different file for different methods like insert,update,delete ,and find
+    // const findResult=await collection.find().toArray();
+    // console.log(findResult);
 }
 
-main();
+module.exports={dbConnection};
