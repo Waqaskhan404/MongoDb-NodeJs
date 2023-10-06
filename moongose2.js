ee3d9d3eab38a42cce629ca08e460eeeb95faed8
Waqas Khan <71628657+Waqaskhan404@users.noreply.github.com>
2023-10-06 23:12:37 +0500

    in moongoose2.js added all the crud operation by Mongoose

---
+++ moongose2.js
@@ -0,0 +1,74 @@
+const express=require("express");
+const mongoose=require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/myDataBase");
+const app=express();
+app.use(express.json());
+
+const BookSchema=new mongoose.Schema({
+    title:String,
+    author:String,
+    age:Number
+});
+
+const Books=mongoose.model("Books",BookSchema);
+
+
+
+app.get("/",(req,res)=>{
+    res.send("<h1>Welcome</h1>");
+})
+
+// get All Books
+app.get("/books",async(req,res)=>{
+    try{
+       const books=await Books.find();
+       res.json(books);
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json("Internal Server Error");
+    }
+});
+
+// Create New Book
+app.post("/books",async(req,res)=>{
+    try{
+        const {title,author,age}=req.body;
+       const newBook=new Books({title,author,age});
+       await newBook.save();
+       res.status(201).json(newBook);
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json("Internal Server Error");
+    }
+});
+
+// Update Book
+
+app.put("/books/:id",async(req,res)=>{
+        const {id}=req.params;
+        const {title,author,age}=req.body;
+       const updateBook=await Books.findByIdAndUpdate(id,{title,author,age});
+       res.json(updateBook);
+});
+
+
+
+// Delete A Book
+app.delete("/books/:id",async(req,res)=>{
+    const {id}=req.params;
+   const deleteBook=await Books.findByIdAndRemove(id);
+   res.sendStatus(204);
+
+});
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
